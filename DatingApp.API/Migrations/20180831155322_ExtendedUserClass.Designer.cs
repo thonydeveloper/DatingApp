ff77feccae691849d66bb36f41721c5c18681e9a
Your Name <you@example.com>
2018-09-05 17:48:03 +0200

    front end users

--- DatingApp.API/Migrations/20180831155322_ExtendedUserClass.Designer.cs
+++ DatingApp.API/Migrations/20180831155322_ExtendedUserClass.Designer.cs
@@ -9,7 +9,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DatingApp.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20180830203651_ExtendedUserClass")]
+    [Migration("20180831155322_ExtendedUserClass")]
     partial class ExtendedUserClass
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
@@ -59,7 +59,7 @@ namespace DatingApp.API.Migrations
 
                     b.Property<string>("Introduction");
 
-                    b.Property<string>("KnowAs");
+                    b.Property<string>("KnownAs");
 
                     b.Property<DateTime>("LastActive");
 
