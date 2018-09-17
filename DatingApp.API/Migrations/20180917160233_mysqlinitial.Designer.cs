fe56db4649ed21a23e73c6d06db4da0105bdff9d
Your Name <you@example.com>
2018-09-17 20:37:48 +0200

    publishing 2

--- DatingApp.API/Migrations/20180917160233_mysqlinitial.Designer.cs
+++ DatingApp.API/Migrations/20180917160233_mysqlinitial.Designer.cs
@@ -9,14 +9,15 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DatingApp.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20180905205431_AddedPublicId")]
-    partial class AddedPublicId
+    [Migration("20180917160233_mysqlinitial")]
+    partial class mysqlinitial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.1.2-rtm-30932");
+                .HasAnnotation("ProductVersion", "2.1.2-rtm-30932")
+                .HasAnnotation("Relational:MaxIdentifierLength", 64);
 
             modelBuilder.Entity("DatingApp.API.Models.Photo", b =>
                 {
