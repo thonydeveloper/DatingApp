9cb12d974f0800519ae1b872a782db5faecf1199
Your Name <you@example.com>
2018-09-19 14:26:14 +0200

    azure publishig

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -37,13 +37,15 @@ namespace DatingApp.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(x => x.
-                UseMySql(Configuration.GetConnectionString("DefaultConnection"))
+                UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
                     .ConfigureWarnings(warnings => warnings.Ignore(CoreEventId.IncludeIgnoredWarning))
                 );
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
                 .AddJsonOptions(opt => {
                     opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
                 });
+
+            services.BuildServiceProvider().GetService<DataContext>().Database.Migrate();
             services.AddCors();
             services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings"));
             services.AddAutoMapper();
@@ -115,7 +117,7 @@ namespace DatingApp.API
             }
 
             // app.UseHttpsRedirection();
-            //seeder.SeedUsers();
+          //  seeder.SeedUsers();
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseAuthentication();
             app.UseDefaultFiles();
